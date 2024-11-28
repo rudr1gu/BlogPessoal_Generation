0b44a42e4c9adf09b5a1380a085058eeac14022b
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-11-28 08:29:16 -0300

    :recycle: Refactor Postagem class to consolidate Lombok annotations for cleaner code

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -12,30 +12,24 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "tb_postagens")
 public class Postagem {
-
-    @Getter
-    @Setter
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
-    @Getter
-    @Setter
+    
     @NotBlank
     @Size(min = 5, max = 100)
     private String titulo;
-
-    @Getter
-    @Setter
+    
     @NotBlank
     @Size(min = 5, max = 100)
     private String texto;
 
-    @Getter
-    @Setter
     @UpdateTimestamp
     private String data;
   
