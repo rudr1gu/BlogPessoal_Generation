5584920ea6a24efd06534cba734f80ac05825172
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-12-06 14:13:29 -0300

    :sparkles: Add @Component annotation to JwtAuthFilter and refactor JwtService for improved key management

--- src/main/java/com/generation/blogpessoal/security/JwtService.java
+++ src/main/java/com/generation/blogpessoal/security/JwtService.java
@@ -12,23 +12,21 @@ import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtService {
         
-    private static final String SECRET = "12345678";
-
     private Key getSignKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
-        return Keys.hmacShaKeyFor(keyBytes);
+        return Keys.secretKeyFor(SignatureAlgorithm.HS256);
     }
 
     private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder()
-        .setSigningKey(getSignKey())
-        .build().parseClaimsJws(token).getBody();
+            .setSigningKey(getSignKey())
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
     }
 
     public <T> T extractAllClaim(String token, Function<Claims, T> claimsResolver) {
@@ -55,17 +53,16 @@ public class JwtService {
 
     private String createToken(Map<String, Object> claims, String userName) {
         return Jwts.builder()
-        .setClaims(claims)
-        .setSubject(userName)
-        .setIssuedAt(new Date(System.currentTimeMillis()))
-        .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-        .signWith(getSignKey(), SignatureAlgorithm.HS256)
-        .compact();
+            .setClaims(claims)
+            .setSubject(userName)
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+            .signWith(getSignKey(), SignatureAlgorithm.HS256)
+            .compact();
     }
 
     public String generateToken(String userName) {
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userName);
     }
-
 }
